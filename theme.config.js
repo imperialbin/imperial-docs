351692da3fb99238b3c4a764e40c5710b75e4987
devlooskie <hello@looskie.com>
2021-04-04 15:42:24 -0400

    a lot of shittt

--- theme.config.js
+++ theme.config.js
@@ -1,35 +1,61 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/imperialbin/imperial-docs',
+  titleSuffix: ' – IMPERIAL documentation',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">IMPERIAL</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next Site Builder
+        documentation
       </span>
     </>
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
+      <meta name="msapplication-TileColor" content="#161A1E" />
+      <meta name="theme-color" content="#161A1E" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the next site builder" />
-      <meta name="og:description" content="Nextra: the next site builder" />
+      <meta
+        name="description"
+        content="This page contains IMPERIALs API, community projects, and official projects documentation."
+      />
+      <meta
+        name="og:description"
+        content="This page contains IMPERIALs API, community projects, and official projects documentation."
+      />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-      <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+      <meta name="twitter:image" content="/banner.png" />
+      <meta name="twitter:site:domain" content="docs.imperialb.in" />
+      <meta name="twitter:url" content="https://docs.imperialb.in/" />
+      <meta name="og:title" content="IMPERIAL - API Documentation" />
+      <meta name="og:image" content="/banner.png" />
+      <meta name="apple-mobile-web-app-title" content="IMPERIAL Docs" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon-96x96.png" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-96x96.png"
+      />
+      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
   search: true,
@@ -37,5 +63,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>
+  footerText: <>MIT {new Date().getFullYear()} © IMPERIAL.</>,
 }
