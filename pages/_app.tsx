111b2057e46157358412721a8aeeab6761ad5722
looskie <50378828+Looskie@users.noreply.github.com>
2021-04-04 16:46:53 -0400

    comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'nextra-theme-docs/style.css'
 
+// @ts-ignore Throws an error saying component and pageprops are type any, no idea what type to put them as though.
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
