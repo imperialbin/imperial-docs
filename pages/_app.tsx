cd30f4c7857f9c5db26e201e4a05aa318f8fe5b2
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-19 17:48:42 -0400

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import 'nextra-theme-docs/style.css'
+import "nextra-theme-docs/style.css";
+import type { AppProps } from "next/app";
 
-// @ts-ignore Throws an error saying component and pageprops are type any, no idea what type to put them as though.
-export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
 }
--- theme.config.js
+++ theme.config.js
@@ -1,47 +1,49 @@
 export default {
-  repository: 'https://github.com/imperialbin/imperial-docs',
-  titleSuffix: ' – IMPERIAL documentation',
+  github: "https://github.com/imperialbin/",
+  docsRepositoryBase: "https://github.com/imperialbin/imperial-docs/blob/main",
+  titleSuffix: " – IMPERIAL",
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">IMPERIAL</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        documentation
+        Documentation
       </span>
     </>
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#161A1E" />
-      <meta name="theme-color" content="#161A1E" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta
-        name="description"
-        content="This page contains IMPERIALs API, community projects, and official projects documentation."
-      />
-      <meta
-        name="og:description"
-        content="This page contains IMPERIALs API, community projects, and official projects documentation."
-      />
+      <meta name="description" content="IMPERIAL Documentation" />
+      <meta name="og:description" content="IMPERIAL Documentation" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="/banner.png" />
-      <meta name="twitter:site:domain" content="docs.imperialb.in" />
+      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="twitter:site:domain" content="https://docs.imperialb.in/" />
       <meta name="twitter:url" content="https://docs.imperialb.in/" />
-      <meta name="og:title" content="IMPERIAL - API Documentation" />
-      <meta name="og:image" content="/banner.png" />
-      <meta name="apple-mobile-web-app-title" content="IMPERIAL Docs" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon-96x96.png" />
+      <meta name="og:title" content="IMPERIAL Documentation" />
+      <meta name="og:image" content="https://docs.imperialb.in/og.png" />
+      <meta
+        name="apple-mobile-web-app-title"
+        content="IMPERIAL Documentation"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-icon-180x180.png"
+      />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/favicon-96x96.png"
+        href="/android-icon-192x192.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-96x96.png"
+        href="/favicon-32x32.png"
       />
       <link
         rel="icon"
@@ -53,15 +55,16 @@ export default {
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-96x96.png"
+        href="/favicon-16x16.png"
       />
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
   search: true,
-  prevLinks: false,
-  nextLinks: false,
+  prevLinks: true,
+  nextLinks: true,
   footer: true,
-  footerEditOnGitHubLink: true,
+  footerEditLink: "Edit this page on GitHub",
   footerText: <>MPL-2.0 {new Date().getFullYear()} © IMPERIAL.</>,
-}
+  unstable_faviconGlyph: "👋",
+};
